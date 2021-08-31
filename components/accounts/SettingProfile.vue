e036f9bd5538d236ffcbbcb422bb43f5a7356458
Anh Thang Bui <buianhthang89@gmail.com>
2021-08-31 21:42:30 +0700

    fix: update settings page, prepare for color mode feature

---
+++ components/accounts/SettingProfile.vue
@@ -0,0 +1,107 @@
+<template>
+  <a-card title="Public profile" :bordered="false" size="small">
+    <a-row>
+      <a-col :xs="24" :sm="16">
+        <a-form-item label="Name">
+          <a-input v-model="user.displayName" disabled>
+            <a-icon slot="prefix" type="user" />
+          </a-input>
+        </a-form-item>
+        <a-form-item label="Email">
+          <a-input v-model="user.email" disabled>
+            <a-icon slot="prefix" type="mail" />
+            <a-icon
+              v-if="user.emailVerified"
+              slot="suffix"
+              type="check-circle"
+              class="email-verified"
+            />
+          </a-input>
+        </a-form-item>
+      </a-col>
+      <a-col :xs="0" :sm="8">
+        <img class="avatar" :src="user.photoURL" />
+      </a-col>
+    </a-row>
+
+    <a-row>
+      <a-col :xs="24" :sm="16">
+        <a-form-item label="Reddit">
+          <a-input v-model="settings.social.reddit" placeholder="u/username">
+            <a-icon slot="prefix" type="reddit" />
+          </a-input>
+        </a-form-item>
+        <a-form-item label="Discord">
+          <a-input v-model="settings.social.discord" placeholder="Discord#0000">
+            <a-icon slot="prefix" :component="DiscordSvg" class="custom-icon" />
+          </a-input>
+        </a-form-item>
+        <a-form-item label="QQ">
+          <a-input v-model="settings.social.qq" placeholder="00000000">
+            <a-icon slot="prefix" type="qq" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            icon="save"
+            type="primary"
+            :loading="loading"
+            @click="saveSettings('social')"
+          >
+            Save
+          </a-button>
+        </a-form-item>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import DiscordSvg from '@/components/icons/DiscordSvg'
+
+export default {
+  data() {
+    return {
+      DiscordSvg,
+      loading: false,
+      settings: {
+        social: {},
+      },
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  beforeMount() {
+    this.settings.social = this.user.social
+  },
+  methods: {
+    saveSettings(type) {
+      this.loading = true
+      this.$fire.firestore
+        .collection('users')
+        .doc(this.user.uid)
+        .update({
+          [type]: this.settings[type],
+        })
+        .then(() => {
+          this.$message.success('Settings updated successfully.')
+          this.loading = false
+        })
+        .catch((e) => {
+          this.$message.success(e.message)
+          this.loading = false
+        })
+    },
+  },
+}
+</script>
+
+<style lang="less">
+.avatar {
+  margin: 0 auto;
+  border-radius: 50%;
+  display: block;
+}
+</style>
--- pages/account/settings.vue
+++ pages/account/settings.vue
@@ -1,83 +1,26 @@
 <template>
   <div class="container">
     <a-page-header title="Settings">
-      <a-tabs default-active-key="general" :tab-position="tabPosition">
-        <a-tab-pane key="general" tab="General">
-          <a-row>
-            <a-col :xs="24" :sm="16">
-              <a-form-item label="Name">
-                <a-input v-model="user.displayName" disabled>
-                  <a-icon slot="prefix" type="user" />
-                </a-input>
-              </a-form-item>
-              <a-form-item label="Email">
-                <a-input v-model="user.email" disabled>
-                  <a-icon slot="prefix" type="mail" />
-                  <a-icon
-                    v-if="user.emailVerified"
-                    slot="suffix"
-                    type="check-circle"
-                    class="email-verified"
-                  />
-                </a-input>
-              </a-form-item>
-            </a-col>
-            <a-col :xs="0" :sm="8">
-              <img class="avatar" :src="user.photoURL" />
-            </a-col>
-          </a-row>
-        </a-tab-pane>
-        <a-tab-pane key="social" tab="Social Profiles">
-          <a-row>
-            <a-col :xs="24" :sm="16">
-              <a-form-item label="Reddit">
-                <a-input
-                  v-model="settings.social.reddit"
-                  placeholder="u/username"
-                >
-                  <a-icon slot="prefix" type="reddit" />
-                </a-input>
-              </a-form-item>
-              <a-form-item label="Discord">
-                <a-input
-                  v-model="settings.social.discord"
-                  placeholder="Discord#0000"
-                >
-                  <a-icon
-                    slot="prefix"
-                    :component="DiscordSvg"
-                    class="custom-icon"
-                  />
-                </a-input>
-              </a-form-item>
-              <a-form-item label="QQ">
-                <a-input v-model="settings.social.qq" placeholder="00000000">
-                  <a-icon slot="prefix" type="qq" />
-                </a-input>
-              </a-form-item>
-              <a-form-item>
-                <a-button
-                  icon="save"
-                  type="primary"
-                  :loading="loading"
-                  @click="saveSettings('social')"
-                >
-                  Save
-                </a-button>
-              </a-form-item>
-            </a-col>
-          </a-row>
-        </a-tab-pane>
-      </a-tabs>
+      <a-card>
+        <a-tabs default-active-key="profile" :tab-position="tabPosition">
+          <a-tab-pane key="profile" tab="Profile">
+            <setting-profile />
+          </a-tab-pane>
+          <a-tab-pane key="appearance" tab="Appearance">
+            <setting-appearance />
+          </a-tab-pane>
+        </a-tabs>
+      </a-card>
     </a-page-header>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import DiscordSvg from '@/components/icons/DiscordSvg'
+import SettingAppearance from '~/components/accounts/SettingAppearance.vue'
+import SettingProfile from '~/components/accounts/SettingProfile.vue'
 
 export default {
+  components: { SettingAppearance, SettingProfile },
   middleware({ store, redirect }) {
     // If the user is not authenticated
     if (!store.state.user.emailVerified) {
@@ -87,11 +30,6 @@ export default {
   data() {
     return {
       tabPosition: this.$device.isMobile ? 'top' : 'left',
-      DiscordSvg,
-      loading: false,
-      settings: {
-        social: {},
-      },
     }
   },
   head() {
@@ -99,38 +37,5 @@ export default {
       title: `Account Settings - ${process.env.appName}`,
     }
   },
-  computed: {
-    ...mapState(['user']),
-  },
-  beforeMount() {
-    this.settings.social = this.user.social
-  },
-  methods: {
-    saveSettings(type) {
-      this.loading = true
-      this.$fire.firestore
-        .collection('users')
-        .doc(this.user.uid)
-        .update({
-          [type]: this.settings[type],
-        })
-        .then(() => {
-          this.$message.success('Settings updated successfully.')
-          this.loading = false
-        })
-        .catch((e) => {
-          this.$message.success(e.message)
-          this.loading = false
-        })
-    },
-  },
 }
 </script>
-
-<style lang="less">
-.avatar {
-  margin: 0 auto;
-  border-radius: 50%;
-  display: block;
-}
-</style>
